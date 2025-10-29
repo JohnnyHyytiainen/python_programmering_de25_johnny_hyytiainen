5197a70085a300a21c8add1eeb240bac6c2e2508
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2025-10-29 12:03:45 +0100

    Lecture 16_unit_testing continuation

--- 16_unit_testing/test_vector.py
+++ 16_unit_testing/test_vector.py
@@ -1,4 +1,4 @@
-from pytest import raises
+from pytest import raises, approx
 from vector import Vector
 
 
@@ -9,15 +9,43 @@ def test_valid_init():
 # Test negative values in init
 
 
+def test_negative_init():
+    v = Vector(-1, -15, 3)
+    assert v.numbers == (-1, -15, 3)
+
+
 # Test string in the init
+def test_string_init_fails():
+    with raises(TypeError):
+        Vector("1")
 
 
 # test len() function
+def test_length_init():
+    v1 = Vector(1, 2, 3)
+    v2 = Vector(2, 3)
+    v3 = Vector(1, 3, 2)
+    assert len(v1) != len(v2)
+    assert len(v3) == len(v1)
+
+    vectors = (Vector(1, 2), Vector(1), Vector(1, 2, 3), Vector(1, 4, 2, 3))
+    expected_lenghts = (2, 1, 3, 4)
+    for a, b in zip(vectors, expected_lenghts):
+        assert len(a) == b
 
 
 # test abs() function
+def test_vector_norm_valid():
+    v = Vector(1, 4)
+    expected_norm = (v[0]**2 + v[1]**2)**.5
+
+    assert abs(v) == approx(expected_norm)
+    assert abs(v) != approx(4.12)
 
 
-def test_empty_vector_fail():
+# def test_absolute_init():
+
+
+def test_empty_vector_fail() -> None:
     with raises(ValueError):
         Vector()
